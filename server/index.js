5e3e0a66a0e00736db77b7b7a4fb6bb9d9a97178
HIMAL L R <117270050+himallr@users.noreply.github.com>
2023-12-07 20:29:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://kitiket-frontend.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use("/user", userRouter)
 app.use("/admin", adminRouter)
 app.use("/movie", movieRouter)
